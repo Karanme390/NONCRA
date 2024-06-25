4c359923a6ebc48d22d8e9cfc7c58fcae9302aa7
Karanme390 <karanme390@gmail.com>
2024-06-25 16:35:13 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const App = () => {
+    return (
+        <><div className='container bg-warning'>
+        <h1>Hello from App component</h1>
+        </div></>
+     );
+}
+ 
+let rootNode= ReactDOM.createRoot(document.getElementById('root'));
+rootNode.render(<App/>);
+export default App
